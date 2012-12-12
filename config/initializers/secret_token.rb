c0a7fa70246c3b59494798b822a4d281971e517f
vaibhav puri <vaibhav@taggito.com>
2012-12-12 12:53:54 -0600

    Project Kickoff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Passlock::Application.config.secret_token = '51da9b55fcce124736846f60e9e20bf982bcd4da662b77802aec6676bb7732131e420fcd105565ec7376048769f083da25ec79e6f6bcca85b92e490fcad98280'
